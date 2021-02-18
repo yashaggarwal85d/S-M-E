b4c4aef2107038f5afb681009977998d85c1578e
srijan0519 <srijan16k@gmail.com>
2021-02-18 13:43:34 +0530

    file load

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform player;
+    Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = player.position + offset;
+        targetPos.x = 0;
+        transform.position = targetPos;
+    }
+}
